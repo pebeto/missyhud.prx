6c04d015a0195c42e9b8128f3c96357a17b3d5ae
Jose Esparza <28990958+pebeto@users.noreply.github.com>
2024-06-05 15:09:15 -0500

    Some eye-candy changes and just retrieving system time by `sceKernelGetSystemTimeLow`

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <pspkernel.h>
 #include <pspdisplay.h>
+#include <pspdisplay_kernel.h>
 #include <pspsysmem_kernel.h>
 #include <psppower.h>
 #include <stdio.h>
@@ -9,8 +10,8 @@
 
 PSP_MODULE_INFO("missyhud", PSP_MODULE_KERNEL, 0, 3);
 
-int (*g_setframebuf)(int unk, void* topaddr, int bufferwidth, int pixelformat,
-    int sync);
+int (*_sceDisplaySetFrameBufferInternal)(int pri, void* topaddr,
+    int bufferwidth, int pixelformat, int sync);
 
 static char active = 1;
 
@@ -22,14 +23,17 @@ int last_fps = -1;
 int getFPS() {
     // Method extracted from PSP-HUD. Thanks to darko79.
     int current_fps = last_fps;
-    SceKernelSysClock clock;
-    sceKernelGetSystemTime(&clock);
-    if((clock.low - fps_last_clock) >= 1000000) {
-        if(fps_last_clock > 0 && fps_last_clock < clock.low) {
-            int el_clock = clock.low - fps_last_clock;
-            if(el_clock > 0) current_fps = (int)((fps_counter - fps_last_counter) * 1000000 / el_clock);
+    u32 clock_low = sceKernelGetSystemTimeLow();
+
+    if((clock_low - fps_last_clock) >= 1000000) {
+        if(fps_last_clock > 0 && fps_last_clock < clock_low) {
+            int el_clock = clock_low - fps_last_clock;
+
+            if(el_clock > 0) {
+                current_fps = (int)((fps_counter - fps_last_counter) * 1000000 / el_clock);
+            }
         }
-        fps_last_clock = clock.low;
+        fps_last_clock = clock_low;
         fps_last_counter = fps_counter;
     }
     last_fps = current_fps;
@@ -41,7 +45,7 @@ int fps = 0;
 void printFps() {
     char msg[16];
     fps = getFPS();
-    sprintf(msg, "FPS: %u", (fps > 0 ? fps : (u8) sceDisplayGetFramePerSec()));
+    sprintf(msg, "FPS: %u", fps);
     blit_string(0, 3, msg, 0xFFFFFF, 0x000000);
 }
 
@@ -58,21 +62,23 @@ void printPowerInfo() {
         blit_string(0, 1, "Power: No battery", 0xFFFFFF, 0x000000);
         return;
     }
-
-    u16 remainingCapacity = scePowerGetBatteryRemainCapacity();
-    u16 fullCapacity = scePowerGetBatteryFullCapacity();
-    u16 percentage = remainingCapacity * 100 / fullCapacity;
-
-    if (scePowerIsBatteryCharging()) {
-        char percentMsgCharging[28];
-        sprintf(percentMsgCharging, "Power: %u%% (charging...)", percentage);
-        blit_string(0, 1, percentMsgCharging, 0xFFFFFF, 0x000000);
-    }
     else {
-        char percentMsgMinutes[27];
-        sprintf(percentMsgMinutes, "Power: %u%% (%u mins)", percentage,
-            (u16) scePowerGetBatteryLifeTime());
-        blit_string(0, 1, percentMsgMinutes, 0xFFFFFF, 0x000000);
+        u16 remainingCapacity = scePowerGetBatteryRemainCapacity();
+        u16 fullCapacity = scePowerGetBatteryFullCapacity();
+        u16 percentage = remainingCapacity * 100 / fullCapacity;
+
+        if (scePowerIsBatteryCharging()) {
+            char percentMsgCharging[28];
+            sprintf(percentMsgCharging, "Power: %u%% (charging...)",
+                percentage);
+            blit_string(0, 1, percentMsgCharging, 0xFFFFFF, 0x000000);
+        }
+        else {
+            char percentMsgMinutes[27];
+            sprintf(percentMsgMinutes, "Power: %u%% (%u mins)", percentage,
+                (u16) scePowerGetBatteryLifeTime());
+            blit_string(0, 1, percentMsgMinutes, 0xFFFFFF, 0x000000);
+        }
     }
 }
 
@@ -83,17 +89,19 @@ void printCpuBusFrequencies() {
     blit_string(0, 2, msg, 0xFFFFFF, 0x000000);
 }
 
-int setframebuf_hook(int unk, void* topaddr, int bufferwidth, int pixelformat,
-    int sync) {
+int sceDisplaySetFrameBufferInternalHook(int pri, void* topaddr,
+    int bufferwidth, int pixelformat, int sync) {
     fps_counter++;
-    return g_setframebuf(unk, topaddr, bufferwidth, pixelformat, sync);
+    return _sceDisplaySetFrameBufferInternal(pri, topaddr, bufferwidth,
+        pixelformat, sync);
 }
 
 int main_thread(unsigned int args, void *argp) {
-    sceKernelDelayThread(1000);
+    sceKernelDelayThread(1000000);
 
-    hook_function((unsigned int*) sceDisplaySetFrameBuf, setframebuf_hook,
-        &g_setframebuf);
+    hook_function((unsigned int*) sceDisplaySetFrameBufferInternal,
+        sceDisplaySetFrameBufferInternalHook,
+        &_sceDisplaySetFrameBufferInternal);
 
     while(active) {
         sceKernelDelayThreadCB(1000/5);
